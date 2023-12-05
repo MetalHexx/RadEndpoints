a2a8816fb61e8490bcc13a447347c061a0eaaf62
William Pereira <MetalHexx@outlook.com>
2023-12-04 21:06:00 -0500

    Added GetExample endpoint

---
+++ MinimalApi/Features/Examples/GetExample/GetExampleEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using MinimalApi.Domain.Examples;
+
+namespace MinimalApi.Features.Examples.GetExample
+{
+    public class GetExampleEndpoint : Endpoint<GetExampleRequest, GetExampleResponse>
+    {
+        private readonly IExampleService _service;
+        public GetExampleEndpoint(IExampleService exampleService) => _service = exampleService;
+        public override void Configure()
+        {
+            Get("/examples/{id}")
+                .Produces<GetExampleResponse>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .AddSwagger(tag: Constants.ExamplesTag, desc: "Get an example by id");                
+        }
+
+        public async override Task<IResult> Handle(GetExampleRequest r, CancellationToken ct)
+        {
+            Response.Example = (await _service.GetExample(r.Id))!;
+            Response.Message = "Example retrieved successfully";
+
+            if(Response.Example is null)
+            {
+                return NotFound("Example not found");
+            }
+            return Ok(Response);
+        }
+    }
+}
---
+++ MinimalApi/Features/Examples/GetExample/GetExampleModels.cs
@@ -0,0 +1,22 @@
+﻿using MinimalApi.Domain.Examples;
+
+namespace MinimalApi.Features.Examples.GetExample
+{
+    public class GetExampleRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetExampleRequestValidator : AbstractValidator<GetExampleRequest>
+    {
+        public GetExampleRequestValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+        }
+    }
+
+    public class GetExampleResponse : EndpointResponse
+    {
+        public Example Example { get; set; } = null!;
+    }
+}
