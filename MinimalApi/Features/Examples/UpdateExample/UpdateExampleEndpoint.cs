02037bdeba869eee34720c2b0c3bcb5098aa5f6d
William Pereira <MetalHexx@outlook.com>
2023-12-04 22:22:34 -0500

    Added update example endpoint

---
+++ MinimalApi/Features/Examples/UpdateExample/UpdateExampleEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using MinimalApi.Domain.Examples;
+
+namespace MinimalApi.Features.Examples.UpdateExample
+{
+    public class UpdateExampleEndpoint : Endpoint<UpdateExampleRequest, UpdateExampleResponse>
+    {
+        private readonly IExampleService _service;
+        public UpdateExampleEndpoint(IExampleService service) => _service = service;
+
+        public override void Configure()
+        {
+            Put("/examples")
+                .Produces<UpdateExampleResponse>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .AddSwagger(tag: Constants.ExamplesTag, desc: "Update an example.");
+        }
+
+        public async override Task<IResult> Handle(UpdateExampleRequest r, CancellationToken ct)
+        {
+            Response.Example = (await _service.UpdateExample(r.Example))!;
+            Response.Message = "Example updated successfully";
+
+            if(Response.Example is null)
+            {
+                return NotFound("Could not find and example with the id provided");
+            }
+            return Ok(Response);
+        }
+    }
+}
---
+++ MinimalApi/Features/Examples/UpdateExample/UpdateExampleModels.cs
@@ -0,0 +1,23 @@
+﻿using MinimalApi.Domain.Examples;
+
+namespace MinimalApi.Features.Examples.UpdateExample
+{
+    public class UpdateExampleRequest
+    {
+        public Example Example { get; set; } = null!;
+    }
+
+    public class UpdateExampleValidator : AbstractValidator<UpdateExampleRequest>
+    {
+        public UpdateExampleValidator()
+        {
+            RuleFor(x => x.Example).NotNull();
+            RuleFor(x => x.Example.Id).GreaterThan(0);
+            RuleFor(x => x.Example).SetValidator(new ExampleValidator());
+        }
+    }
+    public class UpdateExampleResponse : EndpointResponse
+    {
+        public Example Example { get; set; } = null!;
+    }
+}
