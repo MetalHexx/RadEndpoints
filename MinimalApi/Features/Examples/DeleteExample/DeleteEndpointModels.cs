f7308fe958be9b6a40586ffb095f947cecee11b8
William Pereira <MetalHexx@outlook.com>
2023-12-04 20:54:02 -0500

    Added delete endpoint

---
+++ MinimalApi/Features/Examples/DeleteExample/DeleteEndpointModels.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApi.Features.Examples.DeleteExample
+{
+    public class DeleteExampleResponse : EndpointResponse { }
+
+    public class DeleteExampleRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ MinimalApi/Features/Examples/DeleteExample/DeleteExampleEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using MinimalApi.Domain.Examples;
+
+namespace MinimalApi.Features.Examples.DeleteExample
+{
+    public class DeleteExampleEndpoint : Endpoint<DeleteExampleRequest, DeleteExampleResponse>
+    {
+        private readonly IExampleService _service;
+
+        public DeleteExampleEndpoint(IExampleService service) => _service = service;
+        public override void Configure()
+        {
+            Delete("/examples")
+                .Produces<DeleteExampleResponse>(StatusCodes.Status200OK)
+                .AddSwagger(tag: Constants.ExamplesTag, desc: "Delete an example.");
+        }
+
+        public async override Task<IResult> Handle(DeleteExampleRequest r, CancellationToken ct)
+        {
+            await _service.DeleteExample(r.Id);
+            Response.Message = "Example deleted successfully";
+
+            return Ok(Response);
+        }
+    }
+}
