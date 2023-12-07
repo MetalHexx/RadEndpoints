f4c298cf11d37a3c4d983157af22a6b3af9c1f13
William Pereira <MetalHexx@outlook.com>
2023-12-06 19:59:34 -0500

    Added a generic EndpointResponse<T>

--- MinimalApi/Http/Endpoints/EndpointResponse.cs
+++ MinimalApi/Http/Endpoints/EndpointResponse.cs
@@ -4,4 +4,9 @@
     {
         public string Message { get; set; } = string.Empty;
     }
+
+    public class EndpointResponse<T>: EndpointResponse
+    {
+        public T? Data { get; set; } = default!;
+    }
 }
