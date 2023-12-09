d8ec43dffa0165ba1a528a1b63b39724f9331b59
William Pereira <MetalHexx@outlook.com>
2023-12-09 02:47:55 -0500

    Added put / post extensions.  A serializer exception wrapper to make test errors more apparent.

---
+++ MinimalApi.Tests.Integration/Common/EndpointResponseSerializationException.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalApi.Tests.Integration.Common
+{
+    public class EndpointResponseSerializationException : Exception
+    {
+        public HttpResponseMessage HttpResponse { get; set; }
+        public EndpointResponseSerializationException(string stringResponse, HttpResponseMessage httpResponse, Exception innerException) 
+            : base($"\r\nProblem deserializing endpoint response.\r\n  Response Body: {stringResponse} \r\n", innerException) 
+        {
+            HttpResponse = httpResponse;
+        }
+    }
+}
--- MinimalApi.Tests.Integration/Common/HttpClientExtensions.cs
+++ MinimalApi.Tests.Integration/Common/HttpClientExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Endpoint = MinimalApi.Http.Endpoints.Endpoint;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Endpoint = MinimalApi.Http.Endpoints.Endpoint;
 
 namespace MinimalApi.Tests.Integration.Common
 {
@@ -9,9 +13,8 @@ namespace MinimalApi.Tests.Integration.Common
         {
             var route = RouteExtensions.GetAndMapRoute<TEndpoint, TRequest>(request);
             var httpResponse = await client.GetAsync(route);
-            var endpointResponse = await httpResponse.Content.ReadFromJsonAsync<TResponse>();
 
-            return (httpResponse, endpointResponse);
+            return (httpResponse, await httpResponse.DeserializeJson<TResponse>());
         }
 
         public async static Task<(HttpResponseMessage HttpResponse, TResponse? EndpointResponse)> GetAsync<TEndpoint, TResponse>(this HttpClient client)
@@ -19,9 +22,47 @@ namespace MinimalApi.Tests.Integration.Common
         {
             var route = Endpoint.GetRoute<TEndpoint>();
             var httpResponse = await client.GetAsync(route);
-            var endpointResponse = await httpResponse.Content.ReadFromJsonAsync<TResponse>();
 
-            return (httpResponse, endpointResponse);
+            return (httpResponse, await httpResponse.DeserializeJson<TResponse>());
         }
+
+        public async static Task<(HttpResponseMessage HttpResponse, TResponse? EndpointResponse)> DeleteAsync<TEndpoint, TRequest, TResponse>(this HttpClient client, TRequest request)
+            where TEndpoint : Endpoint
+        {
+            var route = RouteExtensions.GetAndMapRoute<TEndpoint, TRequest>(request);
+            var httpResponse = await client.DeleteAsync(route);
+
+            return (httpResponse, await httpResponse.DeserializeJson<TResponse>());
+        }
+
+        public async static Task<(HttpResponseMessage HttpResponse, TResponse? EndpointResponse)> PostAsync<TEndpoint, TRequest, TResponse>(this HttpClient client, TRequest request)
+            where TEndpoint : Endpoint
+        {
+            var route = Endpoint.GetRoute<TEndpoint>();
+            var httpResponse = await client.PostAsJsonAsync(route, request);            
+            return (httpResponse, await httpResponse.DeserializeJson<TResponse>());
+        }
+
+        public async static Task<(HttpResponseMessage HttpResponse, TResponse? EndpointResponse)> PutAsync<TEndpoint, TRequest, TResponse>(this HttpClient client, TRequest request)
+            where TEndpoint : Endpoint
+        {
+            var route = Endpoint.GetRoute<TEndpoint>();
+            var httpResponse = await client.PutAsJsonAsync(route, request);
+
+            return (httpResponse, await httpResponse.DeserializeJson<TResponse>());
+        }
+
+        private static async Task<TResponse?> DeserializeJson<TResponse>(this HttpResponseMessage response)
+        {
+            try
+            {
+                return await response.Content.ReadFromJsonAsync<TResponse>();                
+            }
+            catch (JsonException ex)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+                throw new EndpointResponseSerializationException(stringResponse, response, ex);
+            }
+        } 
     }
 }
