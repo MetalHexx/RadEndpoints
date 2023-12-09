0e781ad91ac94c6c1a979b42e8aa0c3d018880ea
William Pereira <MetalHexx@outlook.com>
2023-12-09 00:57:14 -0500

    Added GetExampleEndpoint tests

--- MinimalApi.Tests.Integration/Tests/Example/GetExampleEndpointTests.cs
+++ MinimalApi.Tests.Integration/Tests/Example/GetExampleEndpointTests.cs
@@ -22,5 +22,38 @@ namespace MinimalApi.Tests.Integration.Tests.Example
             r.Should().NotBeNull();
             r!.Data!.Id.Should().Be(1);
         }
+
+        [Fact]
+        public async void When_GetExampleEndpoint_Called_With_Invalid_Id_Returns_NotFound()
+        {
+            //Act            
+            var (h, r) = await _fixture.Client.GetAsync<GetExampleEndpoint, GetExampleRequest, ProblemDetails>(new()
+            {
+                Id = 999
+            });
+
+            //Assert
+            h.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            r.Should().NotBeNull();
+            r.Should().BeOfType<ProblemDetails>();
+            r!.Title.Should().Be("Example not found");
+            r.Status.Should().Be(StatusCodes.Status404NotFound);
+        }
+        [Fact]
+        public async void When_GetExampleEndpoint_Called_With_Invalid_Id_Returns_ValidationError()
+        {
+            //Act            
+            var (h, r) = await _fixture.Client.GetAsync<GetExampleEndpoint, GetExampleRequest, ProblemDetails>(new()
+            {
+                Id = 0
+            });
+
+            //Assert
+            h.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            r.Should().NotBeNull();
+            r.Should().BeOfType<ProblemDetails>();
+            r!.Title.Should().Be("Validation Error");
+            r.Status.Should().Be(StatusCodes.Status400BadRequest);
+        }
     }
 }
