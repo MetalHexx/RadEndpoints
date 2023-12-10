b622742c678551df0501c4e7fd0910a36e9805b2
William Pereira <MetalHexx@outlook.com>
2023-12-10 00:01:08 -0500

    Added tests for Search Example Endpoint

---
+++ MinimalApi.Tests.Integration/Tests/Example/SearchExampleEndpointTests.cs
@@ -0,0 +1,52 @@
+﻿using MinimalApi.Features.Examples.SearchExamples;
+
+namespace MinimalApi.Tests.Integration.Tests.Example
+{
+    [Collection("Endpoint")]
+    public class SearchExampleEndpointTests(EndpointFixture f)
+    {
+
+        [Theory]
+        [InlineData("Luke", "Skywalker")]
+        [InlineData("", "Skywalker")]
+        [InlineData("Luke", "")]
+        public async Task Given_ExampleExists_WhenCalled_ReturnsSuccess(string firstName, string lastName) 
+        {
+            //Arrange
+            var request = new SearchExamplesRequest
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            //Act
+            var (h, r) = await f.Client.GetAsync<SearchExamplesEndpoint, SearchExamplesRequest, SearchExamplesResponse>(request); 
+
+            //Assert
+            h.StatusCode.Should().Be(HttpStatusCode.OK);
+            r.Should().BeOfType<SearchExamplesResponse>();
+            r!.Data!.First().FirstName.Should().Be("Luke");
+            r.Data!.First().LastName.Should().Be("Skywalker");
+        }
+
+        [Fact]
+        public async Task When_SearchEmpty_ReturnsValidationProblem()
+        {
+            //Arrange
+            var request = new SearchExamplesRequest
+            {
+                FirstName = string.Empty,
+                LastName = string.Empty
+            };
+
+            //Act
+            var (h, r) = await f.Client.GetAsync<SearchExamplesEndpoint, SearchExamplesRequest, ProblemDetails>(request); 
+
+            //Assert
+            h.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            r.Should().BeOfType<ProblemDetails>();
+            r!.Extensions.Should().ContainKey("FirstName");
+            r.Extensions.Should().ContainKey("LastName");
+        }
+    }
+}
