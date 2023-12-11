01ad496b3a9d6fc5e472239a20b5659f2b5dbc32
William Pereira <MetalHexx@outlook.com>
2023-12-10 23:26:43 -0500

    Added RadRequest without T

--- MinimalApi/Http/Endpoints/RadRequest.cs
+++ MinimalApi/Http/Endpoints/RadRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace MinimalApi.Http.Endpoints
 {
-    public abstract class RadRequest<T>
+    public abstract class RadRequest { }
+    public abstract class RadRequest<T> : RadRequest
     {
         public abstract T Data { get; set; }
     }
