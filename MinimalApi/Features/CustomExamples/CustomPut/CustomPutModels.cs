0c2120c6fb0b7e3e74a68a820a7386928c82748d
William Pereira <MetalHexx@outlook.com>
2023-12-10 23:42:05 -0500

    Inheriting from RadRequest

--- MinimalApi/Features/CustomExamples/CustomPut/CustomPutModels.cs
+++ MinimalApi/Features/CustomExamples/CustomPut/CustomPutModels.cs
@@ -1,9 +1,8 @@
-﻿using MinimalApi.Features.CustomExamples._common;
-using MinimalApi.Features.Examples._common;
+﻿using MinimalApi.Features.Examples._common;
 
 namespace MinimalApi.Features.CustomExamples.CustomPut
 {
-    public class CustomPutRequest
+    public class CustomPutRequest: RadRequest
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
