2a811c5f9db79b450fd72708730056a76fb39ae6
William Pereira <MetalHexx@outlook.com>
2023-12-11 00:33:12 -0500

    Only need to create one Fixture class for the entire test run.

--- MinimalApi.Tests.Integration/Common/RadEndpointFixture.cs
+++ MinimalApi.Tests.Integration/Common/RadEndpointFixture.cs
@@ -2,11 +2,14 @@
 {
     public class RadEndpointFixture : IDisposable
     {
-        public HttpClient Client => _factory.CreateClient();
-        public Fixture DataGenerator => new();
+        public HttpClient Client => _factory.CreateClient(); //creating a new client for each test ensure complete test isolation
+        public Fixture DataGenerator { get; private set; } = new();
 
         private readonly WebApplicationFactory<Program> _factory;
-        public RadEndpointFixture() => _factory = new WebApplicationFactory<Program>();
+        public RadEndpointFixture()
+        {
+            _factory = new WebApplicationFactory<Program>();
+        }
         public void Dispose() => _factory.Dispose();
     }
 }
