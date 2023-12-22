d2b4a6625445843adaf63e7224ef93b876f1cff6
William Pereira <MetalHexx@outlook.com>
2023-12-21 22:38:20 -0500

    Removed unnecessary usings

--- RadEndpoints/Validation/ValidationResultExtensions.cs
+++ RadEndpoints/Validation/ValidationResultExtensions.cs
@@ -1,11 +1,4 @@
 ﻿using FluentValidation.Results;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RadEndpoints.Validation
 {
