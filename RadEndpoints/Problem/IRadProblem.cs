95279f0c44c8ee64b9e3ceba55cad61e2546cff6
William Pereira <MetalHexx@outlook.com>
2023-12-22 12:24:22 -0500

    Moved Problems

---
+++ RadEndpoints/Problem/IRadProblem.cs
@@ -0,0 +1,10 @@
+﻿namespace RadEndpoints
+{
+    public interface IRadProblem;
+    public sealed record ForbiddenError(string Message) : IRadProblem;
+    public sealed record ConflictError(string Message) : IRadProblem;
+    public sealed record NotFoundError(string Message) : IRadProblem;
+    public sealed record ValidationError(string Name, string Message) : IRadProblem;
+    public sealed record InternalError(string Message, Exception? Exception) : IRadProblem;
+    public sealed record ExternalError(string Message, Exception? Exception) : IRadProblem;
+}
--- RadEndpoints/Problem/Problem.cs
+++ RadEndpoints/Problem/Problem.cs
@@ -9,11 +9,4 @@
         public static InternalError Internal(string message, Exception? exception = null) => new(message, exception);
         public static ExternalError External(string message, Exception? exception = null) => new(message, exception);
     }
-    public interface IRadProblem;
-    public sealed record ForbiddenError(string Message) : IRadProblem;
-    public sealed record ConflictError(string Message) : IRadProblem;
-    public sealed record NotFoundError(string Message) : IRadProblem;
-    public sealed record ValidationError(string Name, string Message) : IRadProblem;
-    public sealed record InternalError(string Message, Exception? Exception) : IRadProblem;
-    public sealed record ExternalError(string Message, Exception? Exception) : IRadProblem;
 }
