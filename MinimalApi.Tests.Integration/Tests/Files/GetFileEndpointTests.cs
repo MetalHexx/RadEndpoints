831d8ee10b1ac7bcf132df4bdf345d4c2e3c6708
William Pereira <MetalHexx@outlook.com>
2024-01-22 18:32:11 -0500

    Added an example endpoint that sends file bytes with a test.

---
+++ MinimalApi.Tests.Integration/Tests/Files/GetFileEndpointTests.cs
@@ -0,0 +1,34 @@
+﻿using MinimalApi.Features.Files;
+using System.Reflection;
+
+namespace MinimalApi.Tests.Integration.Tests.Files
+{
+    [Collection("Endpoint")]
+    public class GetFileEndpointTests(RadEndpointFixture f)
+    {
+        [Fact]
+        public async void When_RequestValid_ReturnsSuccess()
+        {
+            //Arrange 
+            var expectedBytes = await GetFileBytes(@"Features\File\RadEndpoints.jpg");
+
+            //Act            
+            var r = await f.Client.GetAsync<GetFileEndpoint, GetFileRequest>(new());
+            var actualBytes = await r.Content.ReadAsByteArrayAsync();
+
+            //Assert
+            r.StatusCode.Should().Be(HttpStatusCode.OK);
+            actualBytes.Should().BeEquivalentTo(expectedBytes);
+        }
+
+        private async static Task<byte[]> GetFileBytes(string relativePath)
+        {
+            var exeRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(exeRoot!, relativePath.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar));
+            using var fileStream = File.Open(filePath, FileMode.Open);
+            var bytes = new byte[fileStream.Length];
+            var _ = await fileStream.ReadAsync(bytes.AsMemory(0, (int)fileStream.Length));
+            return bytes;
+        }
+    }
+}
---
+++ MinimalApi/Features/File/GetFileEndpoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Mime;
+using System.Reflection;
+
+namespace MinimalApi.Features.Files
+{
+    public class GetFileRequest : RadRequest { }
+    public class GetFileResponse : RadBytesResponse { }
+    public class GetFileEndpoint : RadEndpoint<GetFileRequest, GetFileResponse>
+    {
+        public override void Configure()
+        {
+            Get("/files")
+                .Produces<GetFileResponse>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithDocument(tag: "Files", desc: "Example of how to send file bytes");
+        }
+
+        public async override Task Handle(GetFileRequest r, CancellationToken ct)
+        {
+            var fileName = "RadEndpoints.jpg";
+
+            Response = new GetFileResponse
+            {
+                Bytes = await GetFileBytes(@$"Features\File\{fileName}"),
+                ContentType = MediaTypeNames.Image.Jpeg,
+                FileDownloadName = fileName,
+                EnableRangeProcessing = false,
+                LastModified = DateTimeOffset.UtcNow
+            };
+            SendBytes(Response);
+        }
+
+        public async static Task<byte[]> GetFileBytes(string relativePath)
+        {
+            var exeRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(exeRoot!, relativePath.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar));
+            using var fileStream = File.Open(filePath, FileMode.Open);
+            var bytes = new byte[fileStream.Length];
+            var _ = await fileStream.ReadAsync(bytes.AsMemory(0, (int)fileStream.Length));
+            return bytes;
+        }
+    }
+}
