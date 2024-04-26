ae93b2df52af2eeea52ecdde176fba86a8c1947a
William Pereira <MetalHexx@outlook.com>
2024-04-25 22:58:52 -0400

    fix: corrections

--- RadEndpoints.Cli/install.bat
+++ RadEndpoints.Cli/install.bat
@@ -1,3 +1,3 @@
 dotnet tool uninstall -g RadEndpoints.Cli
 dotnet pack
-dotnet tool install --global --add-source "C:\dev\src\MinimalApiPoc\RadEndpoints.Cli\nupkg" RadEndpoints.Cli --version 1.0.0
+dotnet tool install --global --add-source .\nupkg RadEndpoints.Cli
