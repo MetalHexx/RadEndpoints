538c733b10c26d53e84b3880c74c68b24c2e19de
William Pereira <MetalHexx@outlook.com>
2024-04-25 23:28:28 -0400

    chore: examples

--- RadEndpoints.Cli/Program.cs
+++ RadEndpoints.Cli/Program.cs
@@ -2,19 +2,20 @@
 using RadEndpoints.Cli.Fonts;
 using RadEndpoints.Cli.Helpers;
 using Spectre.Console.Cli;
-using System.Reflection;
+using Spectre.Console.Cli.Help;
 
 
 RadHelper.RenderLogo("RadEndpoints", FontConstants.FontPath);
 
 var app = new CommandApp();
+app.SetDefaultCommand<GenerateEndpointCommand>();
 
 app.Configure(config =>
 {
     config.SetApplicationName("RadEndpoints.Cli");
     config.SetApplicationVersion("1.0.0");
     config.AddExample(["generate", "endpoint", "GetUser"]);
-    config.AddExample(["g", "e", "GetUser"]);
+    config.AddExample(["generate", "endpoint", "-f", "<DEFINITION_FILE>.json"]);
 
 
     config.AddBranch("generate", generate =>
